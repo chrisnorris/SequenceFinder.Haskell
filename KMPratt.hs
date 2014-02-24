95e3867ed6c8ad2ce34582745e54ec8a842e51d8
chrisnorris <cnorris2007@gmail.com>
2014-02-24 22:36:16 +0000

    - added command line options & ByteString library search (Boyer-Moore)

--- KMPratt.hs
+++ KMPratt.hs
@@ -1,3 +1,6 @@
+-- ./KMPratt 400000  "/Users/OdeMoor/Documents/text2.txt" "l"  +RTS -K20M  -N2 -s -RTS
+-- ghc -O2 KMPratt.hs -rtsopts -threaded -eventlog
+
 import Control.Exception
 import System.Environment
 import qualified Data.ByteString.Lazy as LB
@@ -8,6 +11,7 @@ import Control.Monad.Par
 import Control.DeepSeq
 import Control.Applicative
 import Data.Int
+import Data.ByteString.Search
 
 deep ::  NFData b => b -> b
 deep a = deepseq a a
@@ -25,24 +29,33 @@ matches ws = map fst . filter (ok . snd) . scanl step (0, root)
  grep l [] = Node [] l Null
  grep l (v:vs) = Node (v:vs) l (grep (op l v) vs)
 
-chunk ::  Int64 -> LB.ByteString -> [LB.ByteString]
+chunk ::  Int -> B.ByteString -> [B.ByteString]
 chunk chunkSize bstring =
-    if bstring == LB.empty then [] else
-    let (a, b) = LB.splitAt chunkSize bstring
+    if bstring == B.empty then [] else
+    let (a, b) = B.splitAt chunkSize bstring
     in  a : chunk chunkSize b
 
 searchTarget ::  [Word8]
 searchTarget = [66,65,65,65,65,65,66,65,67,67,67,68,67,67]
 
 main = do
-    ( chunkSizeString : sourceFile : _ ) <- getArgs
-    let chunkSize = read chunkSizeString ::  Int64
-    input <- LB.readFile sourceFile
-    b <- evaluate $ deep
-                  $ runEval
-                  $ parMp (matches searchTarget)
-                  $ LB.unpack <$> chunk chunkSize input
-    mapM_ print $ concat b
+    ( chunkSizeString : sourceFile : flag : _ ) <- getArgs
+    let chunkSize = read chunkSizeString :: Int
+    input <- B.readFile sourceFile
+    case flag of "i" -> do
+                       b <- evaluate $ deep
+                                     $ runEval
+                                     $ parMp (matches searchTarget)
+                                     $ B.unpack <$> chunk chunkSize input
+                       mapM_ print $ concat b
+                 "l" -> do
+                       b <- evaluate $ deep
+                                     $ runEval
+                                     $ parMp (indices $ B.pack searchTarget)
+                                     $ chunk chunkSize input
+                       mapM_ print $ concat b
+
+                 ""  -> return()
 
 parMp ::  (t -> a) -> [t] -> Eval [a]
 parMp f [] = return []
